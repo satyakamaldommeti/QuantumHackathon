2b46358dbb3ce970af54aa4c0c416c5c42723abc
Dommeti Satya Kamal <satyakamal7700sk@gmail.com>
2026-08-22 19:15:11 +0530

    Set base path for deployment in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: '/QuantumHackathon/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
